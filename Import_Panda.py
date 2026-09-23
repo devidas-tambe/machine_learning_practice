799a0ac8e0c2bcc5f69f74a07cfccda78e07bbb6
Devidas tambe <devitambe102@gmail.com>
2026-09-23 12:49:27 +0530

    fill value

--- Import_Panda.py
+++ Import_Panda.py
@@ -11,3 +11,7 @@ print(df)
 print(df.isnull().sum())
 
 print(df.dropna())
+
+df["age"]=df["age"].fillna(df["age"].mean(), inplace=True)
+df["salary"]=df["salary"].fillna(df["salary"].mean(), inplace=True)
+print(df)
